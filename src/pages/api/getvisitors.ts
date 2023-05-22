642cb4e8e279a51a02e70ab42a313fada87a7c2b
kyeryoong <kyeryoong@gmail.com>
2023-05-23 02:53:53 +0900

    env settings. (230523)

--- src/pages/api/getvisitors.ts
+++ src/pages/api/getvisitors.ts
@@ -4,7 +4,7 @@ import { ObjectId } from "mongodb";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const database = (await connectDB).db(process.env.DATABASE);
-    let result = await database.collection("counters").findOne(new ObjectId(process.env.LIKES_ID))
+    let result = await database.collection("counters").findOne(new ObjectId(process.env.COUNTER_ID))
 
     res.status(200).json(result.visitors);
 }
--- src/pages/api/increasevisitors.ts
+++ src/pages/api/increasevisitors.ts
@@ -5,7 +5,7 @@ import { ObjectId } from "mongodb";
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const database = (await connectDB).db(process.env.DATABASE);
     await database.collection("counters").findOneAndUpdate(
-        { _id: new ObjectId(process.env.LIKES_ID) },
+        { _id: new ObjectId(process.env.COUNTER_ID) },
         { $inc: { "visitors": 1 } }
     )
 
